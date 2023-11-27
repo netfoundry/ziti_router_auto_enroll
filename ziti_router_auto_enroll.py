0288a0c0f2e21e956a2cdea86447ecd33a5bb5cd
Edward Moscardini <edward.moscardini@netfoundry.io>
2023-11-27 15:09:26 -0500

    bump version ;

--- ziti_router_auto_enroll.py
+++ ziti_router_auto_enroll.py
@@ -725,7 +725,7 @@ def create_parser():
 
     :return: A Namespace containing arguments
     """
-    __version__ = '1.0.16'
+    __version__ = '1.0.17'
     parser = argparse.ArgumentParser()
 
     add_general_arguments(parser, __version__)
